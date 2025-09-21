4aec4e25cea013d23e73cb8bdc3ff2faa7e95acc
haowern98 <hdwwu69@gmail.com>
2025-09-22 03:56:45 +0800

    Updated vite.config.ts to use https

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      plugins: [basicSsl()],
+      server: {
+        host: '0.0.0.0',
+        port: 5173,
+      },
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
